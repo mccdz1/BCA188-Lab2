b619b2c76f3b7739ccb9150b18f8767b93b0ec01
Michael CADIZ <michael.cadiz@g.msuiit.edu.ph>
2026-09-18 16:23:32 +0800

    Refactor lamp brightness calculation in setup

--- Lab2.ino
+++ Lab2.ino
@@ -1,28 +1,11 @@
-/*
- * Course: BCA188
- * Laboratory Activity 2: Control a Lamp's Brightness
- */
-
-// Function: determines lamp brightness based on switch state
-int getLampBrightness(bool switchOn, int brightnessSetting) {
-  if (switchOn) {
-    return brightnessSetting;
-  } else {
-    return 0;
-  }
-}
-
 void setup() {
   Serial.begin(115200);
+  int brightnessSetting = 75;
+  bool switchOn = true;
+  int lampBrightness = 0;
 
-  // Variables simulating device inputs
-  int brightnessSetting = 75; // Tested values: 0, 50, 100
-  bool switchOn = true;       // Tested values: true (On), false (Off)
+  lampBrightness = getLampBrightness(switchOn, brightnessSetting);
 
-  // Call the function and store the result
-  int lampBrightness = getLampBrightness(switchOn, brightnessSetting);
-
-  // Print results
   Serial.print("Brightness setting: ");
   Serial.println(brightnessSetting);
 
@@ -31,5 +14,12 @@ void setup() {
 }
 
 void loop() {
-  // Empty - simulation runs once in setup()
+}
+
+int getLampBrightness(bool switchOn, int brightnessSetting) {
+  if (switchOn) {
+    return brightnessSetting;
+  } else {
+    return 0;
+  }
 }
